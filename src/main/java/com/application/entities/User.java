32ce222194c289e42d60fbf68b9f7ffe8f8f834d
touzaelhassan <touzaelhassan@gmail.com>
2022-12-29 21:37:23 +0100

    Creating User, Role and Authority entities and relationships between them

---
+++ src/main/java/com/application/entities/User.java
@@ -0,0 +1,101 @@
+package com.application.entities;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userId;
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String email;
+    private String password;
+    private String profileImageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private boolean isActive;
+    private boolean isLocked;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles = new ArrayList<>();
+
+    public User() { }
+
+    public User(String userId, String firstname, String lastname, String username, String email, String password, String profileImageUrl, Date lastLoginDate, Date lastLoginDateDisplay, Date joinDate, boolean isActive, boolean isLocked, List<Role> roles) {
+
+        this.userId = userId;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.profileImageUrl = profileImageUrl;
+        this.lastLoginDate = lastLoginDate;
+        this.lastLoginDateDisplay = lastLoginDateDisplay;
+        this.joinDate = joinDate;
+        this.isActive = isActive;
+        this.isLocked = isLocked;
+        this.roles = roles;
+
+    }
+
+    public User(Long id, String userId, String firstname, String lastname, String username, String email, String password, String profileImageUrl, Date lastLoginDate, Date lastLoginDateDisplay, Date joinDate, boolean isActive, boolean isLocked, List<Role> roles) {
+
+        this.id = id;
+        this.userId = userId;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.profileImageUrl = profileImageUrl;
+        this.lastLoginDate = lastLoginDate;
+        this.lastLoginDateDisplay = lastLoginDateDisplay;
+        this.joinDate = joinDate;
+        this.isActive = isActive;
+        this.isLocked = isLocked;
+        this.roles = roles;
+
+    }
+
+    public void setId(Long id) { this.id = id; }
+    public void setUserId(String userId) { this.userId = userId; }
+    public void setFirstname(String firstname) { this.firstname = firstname; }
+    public void setLastname(String lastname) { this.lastname = lastname; }
+    public void setUsername(String username) { this.username = username; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+    public void setProfileImageUrl(String profileImageUrl) { this.profileImageUrl = profileImageUrl; }
+    public void setLastLoginDate(Date lastLoginDate) { this.lastLoginDate = lastLoginDate; }
+    public void setLastLoginDateDisplay(Date lastLoginDateDisplay) { this.lastLoginDateDisplay = lastLoginDateDisplay; }
+    public void setJoinDate(Date joinDate) { this.joinDate = joinDate; }
+    public void setActive(boolean active) { isActive = active; }
+    public void setLocked(boolean locked) { isLocked = locked; }
+    public void setRoles(List<Role> roles) { this.roles = roles; }
+
+    public Long getId() { return id; }
+    public String getUserId() { return userId; }
+    public String getFirstname() { return firstname; }
+    public String getLastname() { return lastname; }
+    public String getUsername() { return username; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public String getProfileImageUrl() { return profileImageUrl; }
+    public Date getLastLoginDate() { return lastLoginDate; }
+    public Date getLastLoginDateDisplay() { return lastLoginDateDisplay; }
+    public Date getJoinDate() { return joinDate; }
+    public boolean isActive() { return isActive; }
+    public boolean isLocked() { return isLocked; }
+    public List<Role> getRoles() { return roles; }
+
+}
