c4598e1e9a337b4e1e4aa31e08d771a86dabb05c
touzaelhassan <touzaelhassan@gmail.com>
2022-12-29 20:52:04 +0100

    Setup The user management application project

---
+++ src/main/java/com/application/SpringUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUserManagementApplication.class, args);
+    }
+
+}
