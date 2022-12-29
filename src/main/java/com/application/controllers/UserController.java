b5740b9cc8bd8e192001315c804dc03c23bc9698
touzaelhassan <touzaelhassan@gmail.com>
2022-12-29 23:00:50 +0100

    Creating The User controller to test The application

---
+++ src/main/java/com/application/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.application.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/users")
+    public String test(){ return "Hello From The User Endpoint"; }
+
+}
