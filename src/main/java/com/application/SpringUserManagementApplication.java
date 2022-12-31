13701a3c5ff78d4e16db193e624017a8b36c5619
touzaelhassan <touzaelhassan@gmail.com>
2022-12-31 12:01:47 +0100

    Creating Security Configuration Class using WebSecurityConfigurerAdapter

--- src/main/java/com/application/SpringUserManagementApplication.java
+++ src/main/java/com/application/SpringUserManagementApplication.java
@@ -11,7 +11,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -27,11 +26,9 @@ public class SpringUserManagementApplication {
 
     @Bean
     CommandLineRunner run(
-
             AuthorityServiceSpecification authorityServiceBean,
             RoleServiceSpecification roleServiceBean,
             UserServiceSpecification userServiceBean
-
     ){
 
         return args -> {
